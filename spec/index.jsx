7a6c8ba0fab7d62ca3115b3e104e8c42df58866c
Nathan Smith <natsmith@digitalriver.com>
2015-11-23 15:39:40 -0600

    adding react-a11y for accessiblity warnings in console.

--- spec/index.jsx
+++ spec/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './root';
+import a11y from 'react-a11y';
+
+if (process.env.NODE_ENV === 'development') a11y(React);
 
 ReactDOM.render(<Root />, document.getElementById('toolbox-test'));
