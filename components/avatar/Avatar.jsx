a2e92f00d9f92756a7b6478a0483c3390fe059e5
Nathan Smith <natsmith@digitalriver.com>
2015-12-11 11:48:10 -0600

    refactored Avatar.

--- components/avatar/Avatar.jsx
+++ components/avatar/Avatar.jsx
@@ -1,14 +1,34 @@
 import React, {PropTypes} from 'react';
 import FontIcon from '../font_icon';
-import style from './style';
+import style from './style.css';
 
 const Avatar = ({children, className, icon, image, title, ...other}) => {
+
+  if (React.Children.count(children) == 1 && typeof children == 'string'){
+    children = <span className={style.letter}>{children[0]}</span>;
+  }
+
+  let avatar = null;
+
+  if (typeof image === 'string') {
+    avatar = <img className={style.image} src={image} title={title} />
+  } else if (image){
+    avatar = image;
+  } else if (typeof icon === 'string'){
+    avatar = <FontIcon className={style.letter} value={icon} />
+  } else if (icon){
+    avatar = icon;
+  } else if (title) {
+    avatar = <span className={style.letter}>{title[0]}</span>
+  }
+
+  let classes = style.avatar;
+  if (className) classes += ` ${className}`;
+
   return (
-    <div data-react-toolbox='avatar' className={`${style.avatar} ${className}`} {...other}>
+    <div data-react-toolbox='avatar' className={classes} {...other}>
       {children}
-      {typeof image === 'string' ? <img className={style.image} src={image} title={title} /> : image}
-      {typeof icon === 'string' ? <FontIcon className={style.letter} value={icon} /> : icon}
-      {title ? <span className={style.letter}>{title[0]}</span> : null}
+      {avatar}
     </div>
   );
 };
