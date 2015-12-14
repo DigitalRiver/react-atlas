86e179ee210df9e83996d3cfdf8b54644536cd42
Eric Sewalson <esewalson@digitalriver.com>
2015-12-14 14:14:13 -0600

    dialog refactor style

--- components/dialog/Dialog.jsx
+++ components/dialog/Dialog.jsx
@@ -1,35 +1,10 @@
 import React from 'react';
-import ClassNames from 'classnames';
+import classNames from 'classnames/bind';
 import Button from '../button';
 import Overlay from '../overlay';
-import style from './style.scss';
+import style from './style.css';
 
-const Dialog = (props) => {
-  const actions = props.actions.map((action, idx) => {
-    const className = ClassNames(style.button, {[action.className]: action.className});
-    return <Button key={idx} {...action} className={className} />;
-  });
-
-  const className = ClassNames([ style.root, style[props.type] ], {
-    [style.active]: props.active
-  }, props.className);
-
-  return (
-    <Overlay active={props.active} onClick={props.onOverlayClick}>
-      <div data-react-toolbox='dialog' className={className}>
-        <section role='body' className={style.body}>
-          {props.title ? <h6 className={style.title}>{props.title}</h6> : null}
-          {props.children}
-        </section>
-        <nav role='navigation' className={style.navigation}>
-          {actions}
-        </nav>
-      </div>
-    </Overlay>
-  );
-};
-
-Dialog.propTypes = {
+const propTypes = {
   actions: React.PropTypes.array,
   active: React.PropTypes.bool,
   children: React.PropTypes.node,
@@ -39,10 +14,46 @@ Dialog.propTypes = {
   type: React.PropTypes.string
 };
 
-Dialog.defaultProps = {
+const defaultProps = {
   actions: [],
   active: false,
   type: 'normal'
 };
+
+const Dialog = (props) => {
+  const {body, navigation, active, base, button, title, type} = this.props;
+  const cx = classNames.bind(style);
+  const actions = props.actions.map((action, idx) => {
+    const className = cx({
+      button: true,
+      [action.className]: action.className
+    });
+    return <Button key={idx} {...action} className={className} />;
+  });
+
+  let className = cx({
+    base: true,
+    type: props.type,
+    active: props.active,
+    [props.className]: true
+  });
+
+  return (
+      <Overlay active={props.active} onClick={props.onOverlayClick}>
+        <div data-react-toolbox='dialog' className={className}>
+          <section role='body' className={body}>
+            {props.title ? <h6 className={title}>{props.title}</h6> : null}
+            {props.children}
+          </section>
+          <nav role='navigation' className={navigation}>
+            {actions}
+          </nav>
+        </div>
+      </Overlay>
+  );
+};
+
+
+
 
 export default Dialog;
