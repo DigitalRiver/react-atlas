ec559015628b2dc039aac5e372f075a7140bc3c8
Nathan Smith <natsmith@digitalriver.com>
2016-01-12 15:50:54 -0600

    fixing font-icon. making spec build match prd css classes

--- spec/components/avatar.jsx
+++ spec/components/avatar.jsx
@@ -6,7 +6,7 @@ const AvatarTest = () => (
   <section>
     <h5>Avatars</h5>
     <p>Provide an image source or object, a font icon, children or a title to use its first letter.</p>
-    <Avatar style={{backgroundColor: 'deepskyblue'}} icon="folder" />
+    <Avatar style={{backgroundColor: 'deepskyblue'}} ><GithubIcon /></Avatar>
     <Avatar title="Nathan" />
     {/* icon beats title */}
     <Avatar title="Nathan" icon={<GithubIcon />} />
