a972906ead6a37a617abf78a9fc78b4549927e38
Nathan Smith <natsmith@digitalriver.com>
2016-01-20 11:43:04 -0600

    some checkbox changes

--- components/checkbox/Checkbox.jsx
+++ components/checkbox/Checkbox.jsx
@@ -19,16 +19,34 @@ class Checkbox extends Component {
           "block": !inline,
           inline,
           disabled,
+          className
+      });
+
+      return (
+          <label className={componentClass} title={title}>
+              <input {...props} type="checkbox" disabled={disabled} className={style.input} />
+              {label && <span className={style.label}>{label}</span>}
+          </label>
+      );
+  }
+}
+
+const Checkbox = ({title, label, disabled, inline, className, ...other}) => {
+      const cx = ClassNames.bind(style);
+      const componentClass = cx({
+          "block": !inline,
+          inline,
+          disabled,
           [className]: className
       });
 
       return (
           <label className={componentClass} title={title}>
-              <input type="checkbox" disabled={disabled} className={style.input} {...props} />
+              <input {...other} type="checkbox" disabled={disabled} className={style.input} />
               {label && <span className={style.label}>{label}</span>}
           </label>
       );
-  }
+
 }
 
 Checkbox.propTypes = propTypes;
