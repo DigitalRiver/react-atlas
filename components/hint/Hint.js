b940dea42d0a15b038b9f3f461cab1a5a5592261
Nathan Smith <natsmith@digitalriver.com>
2016-01-26 10:34:40 -0600

    adding hint component

---
+++ components/hint/Hint.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './hint.css';
+
+const Hint = ({className, children, ...props}) => {
+	let classNames = style.base;
+	if (className) classNames += ` ${className}`;
+	return (
+		<span {...props} className={classNames}>{children}</span>
+	)
+};
+
+
+export default Hint;
