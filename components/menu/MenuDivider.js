5598abd49c1bd2e0ad0b18f9019eadddd9ac7bce
Nathan Smith <natsmith@digitalriver.com>
2016-02-08 17:00:34 -0600

    removing menu and navigation components

--- components/menu/MenuDivider.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './style.menu_divider';
-
-const MenuDivider = () => (
-  <hr data-react-toolbox='menu-divider' className={style.root}/>
-);
-
-export default MenuDivider;
--- components/menu/index.js
+++
@@ -1,4 +0,0 @@
-export Menu from './Menu';
-export MenuItem from './MenuItem';
-export MenuDivider from './MenuDivider';
-export IconMenu from './IconMenu';
