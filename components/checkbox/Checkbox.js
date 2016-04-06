ddaf8534f2f66cb17cc15cf82375ab6a4bfe658d
Nathan Smith <natsmith@digitalriver.com>
2016-04-06 14:33:34 -0500

    Checkbox docs

--- components/checkbox/Checkbox.js
+++ components/checkbox/Checkbox.js
@@ -2,6 +2,9 @@ import React, { Component, PropTypes } from 'react';
 import ClassNames from 'classnames/bind';
 import style from './checkbox.css';
 
+/**
+ * Simple component for a basic checkbox
+ */
 const Checkbox = ({title, label, disabled, inline, className, ...other}) => {
       const cx = ClassNames.bind(style);
       const componentClass = cx({
@@ -10,6 +13,8 @@ const Checkbox = ({title, label, disabled, inline, className, ...other}) => {
           disabled,
           [className]: className
       });
+
+      title = title ? title : label;
 
       return (
           <label className={componentClass} title={title}>
@@ -20,12 +25,73 @@ const Checkbox = ({title, label, disabled, inline, className, ...other}) => {
 };
 
 Checkbox.propTypes = {
-    className: PropTypes.string,
-    disabled: PropTypes.bool,
-    inline: PropTypes.bool,
-    label: PropTypes.string,
-    style: PropTypes.object,
-    title: PropTypes.string
+  /**
+   * Text for checkbox label
+   * @examples 'Some Label'
+   */
+  label: PropTypes.string,
+	/**
+   * A css class name that will be appended to the wrapping <label> element around the <input> and <span> elements.
+   */
+  className: PropTypes.string,
+	/**
+   * If included, checkbox is disabled
+   * @examples <Checkbox disabled />, <Checkbox disabled={true} />
+   */
+  disabled: PropTypes.bool,
+	/**
+   * If included, renders the checkbox and it's label inline, so it can be side-by-side to other content.
+   */
+  inline: PropTypes.bool,
+  /**
+   * Text for checkbox label title. (i.e. "alt-text" for checkboxes, useful for accessibility). If not provided, will be label text.
+   * @examples 'Some Title'
+   */
+  title: PropTypes.string,
+	/**
+   * Defines if checkbox should be checked on load.
+   */
+  defaultChecked: PropTypes.bool
+};
+
+Checkbox.defaultProps = {
+  className: '',
+  disabled: false,
+  inline: false
+};
+
+Checkbox.styleguide = {
+  category: "Form Components",
+  index: "3.2",
+  example: `
+<section style={{textAlign:"left"}}>
+  <h4 style={{marginBottom: "10px"}}>Checkboxes</h4>
+  <Checkbox
+    defaultChecked={true}
+    label="Checked"
+  />
+  <Checkbox
+    defaultChecked={false}
+    label="Not checked"
+
+  />
+  <Checkbox
+    defaultChecked={true}
+    label="Disabled checkbox"
+    disabled
+  />
+  <Checkbox
+    defaultChecked={false}
+    label="Inline checkbox 1"
+    inline
+  />
+  <Checkbox
+    defaultChecked={false}
+    label="Inline checkbox 2"
+    inline
+  />
+</section>
+`
 };
 
 export default Checkbox;
