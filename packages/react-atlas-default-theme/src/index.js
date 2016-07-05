b3dc4e83e329777f49fe81305db6451f32e92fa2
Juan Francisco Marino <marinojuanfrancisco@gmail.com>
2016-07-04 22:44:00 -0300

    Extracted styles from Slider component, need to fix unique children key issue

--- packages/react-atlas-default-theme/src/index.js
+++ packages/react-atlas-default-theme/src/index.js
@@ -7,3 +7,4 @@ export WarningButton from './button/WarningButton';
 export DangerButton from './button/DangerButton';
 export LinkButton from './button/LinkButton';
 export Hint from './hint';
+export Slider from './slider';
