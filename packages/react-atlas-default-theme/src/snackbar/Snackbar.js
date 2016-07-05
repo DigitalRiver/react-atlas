0f6b47bcf54166c9421e40377db6cdfd966b4def
Juan Francisco Marino <marinojuanfrancisco@gmail.com>
2016-07-04 22:56:09 -0300

    Abstracted styles from Snackbar

---
+++ packages/react-atlas-default-theme/src/snackbar/Snackbar.js
@@ -0,0 +1,5 @@
+import { Snackbar } from 'react-atlas-core';
+import style from './snackbar.css';
+import createComponent from '../createComponent';
+
+export default createComponent(Snackbar, style);
