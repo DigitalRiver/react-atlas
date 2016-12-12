1cb835846739b424551f7adce813abc1fbf10379
Harry <hbowden@digitalriver.com>
2016-12-12 11:28:36 -0600

    Have avatar component fallback on other props if an image fails to load.
    Also add a defaultImage prop so user's can set a defaultImage to fallback to if the main image fails to load. This commit also adds the associated tests and documentation for the avatar component.

--- packages/react-atlas-core/src/avatar/Avatar.js
+++ packages/react-atlas-core/src/avatar/Avatar.js
@@ -6,98 +6,152 @@ import themeable from 'react-themeable';
  *
  * **NOTE**: children will always take precedence over props passed into component.
  */
-const Avatar = ({children, icon, image, title, ...props}) => {
-  const theme = themeable(props.theme);
-  let kids = children;
-  if (React.Children.count(children) === 1 && typeof children === "string"){
-    kids = <span {...theme(4, 'letter')}>{children[0]}</span>;
+  export default class Avatar extends React.Component {
+    constructor(props) {
+      super(props);
+
+      let image;
+
+      if(typeof props.image === "undefined") {
+        if(typeof props.defaultImage === "undefined") {
+          image = null;
+        } else {
+          image = props.defaultImage;
+        }
+      } else {
+        image = props.image;
+      }
+
+      this.state = {image: image};
+    }
+
+    handleBadImage() {
+      /* If the default Image is equal to the bad image URL or the default image is undefined
+        set this.state.image as null so avatar will fallback on a different prop. */
+      if(this.props.defaultImage === this.state.image || typeof this.props.defaultImage === 'undefined') {
+         this.setState({image: null});
+         return;
+      }
+
+      this.setState({image: this.props.defaultImage});
+    }
+
+    render() {
+      const theme = themeable(this.props.theme);
+      var {children, icon, title} = this.props;
+      let kids = children;
+      if (React.Children.count(children) === 1 && typeof children === "string"){
+        kids = <span {...theme(4, 'letter')}>{children[0]}</span>;
+      }
+
+      let avatar = null;
+      var image = this.state.image;
+
+      if (typeof image === "string") {
+        avatar = <img src={image} title={title} onError={this.handleBadImage.bind(this)} {...theme(3, 'image')} />;
+      } else if (image) {
+        avatar = image;
+      } else if (icon) {
+        avatar = icon;
+      } else if (title) {
+        avatar = <span {...theme(2, 'letter')}>{title[0]}</span>;
+      }
+
+      return (
+        <div {...theme(1, 'avatar')}>
+          {kids}
+          {avatar}
+        </div>
+      );
+    }
   }
 
-  let avatar = null;
+ Avatar.propTypes = {
+   /**
+    * Children should be either a string, an icon/glyphicon, or an image tag.
+    * @examples "SomeName", <SomeIcon />, <img src="/path/to/image.jpg"/>
+    */
+   children: PropTypes.node,
+   /**
+    * A css class name that will be appended to the wrapping div around the avatar.
+    */
+   className: PropTypes.string,
+   /**
+    * For displaying an icon/glphyicon. Normally these will be another component or an element with a class on it.
+    * @examples <GithubIcon />, <i class="fa fa-github"></i>
+    */
+   icon: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+   /**
+    * Path to an image
+    * @examples "http://path.to/an/image.jpg"
+    */
+   image: PropTypes.string,
+   /**
+    * A string. Avatar will use First letter of the string.
+    * @examples "Nathan" will output "N"
+    */
+   title: PropTypes.string,
 
-  if (typeof image === "string") {
-    avatar = <img src={image} title={title} {...theme(3, 'image')} />;
-  } else if (image){
-    avatar = image;
-  } else if (icon){
-    avatar = icon;
-  } else if (title) {
-    avatar = <span {...theme(2, 'letter')}>{title[0]}</span>;
-  }
+   /**
+    * A URL to a image that is displayed when the main image fails to load.
+    */
+   defaultImage: PropTypes.string,
 
-  return (
-    <div {...props} {...theme(1, 'avatar')}>
-      {kids}
-      {avatar}
-    </div>
-  );
-};
+   theme: React.PropTypes.shape({
+     letter: React.PropTypes.string,
+     image: React.PropTypes.string,
+     avatar: React.PropTypes.string
+   })
+ };
 
-Avatar.propTypes = {
-  /**
-   * Children should be either a string, an icon/glyphicon, or an image tag.
-   * @examples "SomeName", <SomeIcon />, <img src="/path/to/image.jpg"/>
-   */
-  children: PropTypes.node,
-  /**
-   * A css class name that will be appended to the wrapping div around the avatar.
-   */
-  className: PropTypes.string,
-  /**
-   * For displaying an icon/glphyicon. Normally these will be another component or an element with a class on it.
-   * @examples <GithubIcon />, <i class="fa fa-github"></i>
-   */
-  icon: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
-  /**
-   * Path to an image
-   * @examples "http://path.to/an/image.jpg"
-   */
-  image: PropTypes.string,
-  /**
-   * A string. Avatar will use First letter of the string.
-   * @examples "Nathan" will output "N"
-   */
-  title: PropTypes.string
-};
+ Avatar.defaultProps = {
+   className: '',
+   icon: '',
+   theme: {
+     letter: "ra_avatar__letter",
+     image: "ra_avatar__image",
+     avatar: "ra_avatar__avatar"
+   }
+ };
 
-Avatar.defaultProps = {
-  className: '',
-  icon: ''
-};
-
-Avatar.styleguide = {
-  category: 'Avatar',
-  index: '2.1',
-  example: `
-  <section>
-    <h5>Avatars</h5>
-    {/* background color change */}
-    <Avatar style={{backgroundColor: 'deepskyblue'}} >
-      <i className="fa fa-github"></i>
-    </Avatar>
-    {/* title prop gets truncated to 1st letter */}
-    <Avatar title="Nathan" />
-    {/* icon beats title */}
-    <Avatar title="Nathan" icon={<i className="fa fa-github"></i>} />
-    {/* image beats icon */}
-    <Avatar 
-      icon={<i className="fa fa-github"></i>}
-      image="cat.jpg"
-    />
-    {/* image beats title */}
-    <Avatar title="Javier" image="cat.jpg" />
-    {/* child beats parameters */}
-    <Avatar title="Nathan" image="cat.jpg">
-      <i className="fa fa-github"></i>
-    </Avatar>
-    <Avatar title="Nathan" icon={<i className="fa fa-github"></i>}>
-      <img src="cat.jpg"/>
-    </Avatar>
-    {/* child string gets truncated to 1st letter */}
-    <Avatar>Nathan</Avatar>
-    {/* child should be <i>, <svg>, <img>, or string */}
-    <Avatar><i className="fa fa-github"></i></Avatar>
-  </section>
-  `
-};
-export default Avatar;
+ Avatar.styleguide = {
+   category: 'Avatar',
+   index: '2.1',
+   example: `
+   <section>
+     <h5>Avatars</h5>
+     {/* background color change */}
+     <Avatar style={{backgroundColor: 'deepskyblue'}} >
+       <i className="fa fa-github"></i>
+     </Avatar>
+     {/* title prop gets truncated to 1st letter */}
+     <Avatar title="Nathan" />
+     {/* icon beats title */}
+     <Avatar title="Nathan" icon={<i className="fa fa-github"></i>} />
+     {/* image beats icon */}
+     <Avatar
+       icon={<i className="fa fa-github"></i>}
+       image="cat.jpg"
+     />
+     {/* image beats title */}
+     <Avatar title="Javier" image="cat.jpg" />
+     {/* child beats parameters */}
+     <Avatar title="Nathan" image="cat.jpg">
+       <i className="fa fa-github"></i>
+     </Avatar>
+     <Avatar title="Nathan" icon={<i className="fa fa-github"></i>}>
+       <img src="cat.jpg"/>
+     </Avatar>
+     {/* image beats defaultImage */}
+     <Avatar image="nature.jpg" defaultImage="cat.jpg" />
+     {/* defaultImage beats title */}
+     <Avatar title="Javier" defaultImage="nature.jpg" />
+     {/* defaultImage will replace a image that fails to load */}
+     <Avatar image="badImage.jpg" defaultImage="chillgirl.jpeg" />
+     {/* child string gets truncated to 1st letter */}
+     <Avatar>Nathan</Avatar>
+     {/* child should be <i>, <svg>, <img>, or string */}
+     <Avatar><i className="fa fa-github"></i></Avatar>
+   </section>
+   `
+ };
---
+++ packages/react-atlas-core/src/avatar/__tests__/avatar.test.js
@@ -0,0 +1,52 @@
+
+import React from 'react'
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+import Avatar from '../Avatar.js'
+
+let title = "testTitle";
+let image = "picture.jpg";
+let icon = 'icon={<i className="fa fa-github"></i>}';
+
+describe('Testing Avatar component', () => {
+  it('Set props should match what was passed in', function() {
+    const result = mount(<Avatar title={title} image={image} icon={icon}/>);
+    expect(result.props().title).to.equal(title);
+    expect(result.props().image).to.equal(image);
+    expect(result.find('img').length).to.equal(1);
+    expect(result.props().icon).to.equal(icon);
+  });
+
+  it('If image is not set then state.image should be equal to defaultImage', function() {
+    const result = mount(<Avatar defaultImage={image}/>);
+    expect(result.state().image).to.equal(image);
+  });
+
+  it('If image and defaultImage are not set then state.image should be equal to null', function() {
+    const result = mount(<Avatar/>);
+    expect(result.state().image).to.equal(null);
+  });
+
+  it('If an image is not set and an icon is set then avatar should have the icon as a child.', function() {
+    const result = shallow(<Avatar icon={icon}/>);
+    expect(result.state().image).to.equal(null);
+    expect(result.type()).to.equal('div');
+    expect(result.contains(icon)).to.equal(true);
+  });
+
+  it('If image is set but fails to load fall back on the default image.', function() {
+    const result = mount(<Avatar image={"incorrect.jpg"} defaultImage={image} />);
+    var img = result.find('img');
+    img.simulate('error');
+    expect(result.state().image).to.equal(image);
+  });
+
+  it('The avatar component should fall back on the title prop if image and defaultImage props are not set or fail to load.', function() {
+    const result = mount(<Avatar image={image} title={title}/>);
+    var img = result.find('img');
+    img.simulate('error');
+    expect(result.state().image).to.equal(null);
+    let child = result.children().at(0);
+    expect(child.text()).to.equal(title[0]);
+  });
+});
