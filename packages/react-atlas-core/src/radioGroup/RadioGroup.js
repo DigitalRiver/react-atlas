ae9914accd4846caaeb0a5bd5f0f7ce4b02cd078
Eric Sewalson <esewalson@digitalriver.com>
2017-03-16 15:53:07 -0500

    changed styleName to className in radioGroup

--- packages/react-atlas-core/src/radioGroup/RadioGroup.js
+++ packages/react-atlas-core/src/radioGroup/RadioGroup.js
@@ -4,7 +4,7 @@ import cx from 'classNames';
 
 const RadioGroup = ({ className, children, name, ...props }) => {
   return (
-    <div {...props} styleName={className}>
+    <div {...props} className={className}>
       {React.Children.map(children, child => {
         if (child.type === Radio) {
           return <Radio {...child.props} name={name}/>;
