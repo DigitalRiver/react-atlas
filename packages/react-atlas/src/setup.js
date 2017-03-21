5e749502330b411bd0c01e327a2d90dd8cd5aa0e
Harry Bowden <hbowden@digitalriver.com>
2017-03-21 13:56:14 -0500

    Add newline, warning message and refactor code.

--- packages/react-atlas/src/setup.js
+++ packages/react-atlas/src/setup.js
@@ -1,10 +1,35 @@
 /* This file consumes an atlas.config.js file and generates an index.js for react-atlas. */
 let dot = require('dot');
 let fs = require('fs');
+let eol = require('os').EOL;
 
 const cwd = process.cwd();
 const path = cwd + '/atlas.config.js';
-const template = "import CSSModules from 'react-css-modules'; {{~it.dependencies :value:index}} import { {{=value.name}}Core } from 'react-atlas-core'; import { {{=value.name}}Style } from '{{=value.theme}}';  export const {{=value.name}} = CSSModules({{=value.name}}Core, {{=value.name}}Style, {allowMultiple: true}); {{~}}";
+const warningMessage = "/* WARNING, THIS FILE WAS MACHINE GENERATED. DO NOT MODIFY THIS FILE DIRECTLY " + eol +
+                       "BECAUSE YOUR CHANGES WILL BE OVERWRITTEN WHEN THIS FILE IS GENERATED AGAIN. " + eol +
+                       "IF YOU WAN'T TO MODIFY THIS FILE YOU SHOULD BE MODIFYING THE GENERATOR IT'S SELF " + eol +
+                       "AND REGENERATE THIS FILE. */" + eol;
+
+let template = warningMessage;
+template += "import CSSModules from 'react-css-modules';" + eol + 
+            "{{~it.dependencies :value:index}}" + eol +
+            "import { {{=value.name}}Core } from 'react-atlas-core';" + eol + 
+            "import { {{=value.name}}Style } from '{{=value.theme}}';" + eol +
+            "export const {{=value.name}} = CSSModules({{=value.name}}Core, {{=value.name}}Style, {allowMultiple: true}); {{~}}" + eol;
+
+dot.templateSettings = {
+  evaluate:    /\{\{([\s\S]+?)\}\}/g,
+  interpolate: /\{\{=([\s\S]+?)\}\}/g,
+  encode:      /\{\{!([\s\S]+?)\}\}/g,
+  use:         /\{\{#([\s\S]+?)\}\}/g,
+  define:      /\{\{##\s*([\w\.$]+)\s*(\:|=)([\s\S]+?)#\}\}/g,
+  conditional: /\{\{\?(\?)?\s*([\s\S]*?)\s*\}\}/g,
+  iterate:     /\{\{~\s*(?:\}\}|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*\}\})/g,
+  varname: 'it',
+  strip: false,
+  append: true,
+  selfcontained: false
+};
 
 /* TODO: Replace hardcoded array with a dynamic solution. */
 let components = ['autocomplete', 'avatar', 'button', 'card', 'checkbox',
