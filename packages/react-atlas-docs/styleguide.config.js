47cba0428d668cb09c60eb455df456602daa85cb
cnoree <cnoree@digitalriver.com>
2017-04-04 11:51:53 -0500

    update comments for styleguide.config.js

--- packages/react-atlas-docs/styleguide.config.js
+++ packages/react-atlas-docs/styleguide.config.js
@@ -6,10 +6,10 @@ module.exports = {
 		path.join(__dirname, 'src/assets/reactAtlas.min.css')
 	],
   // Use this to select all components in the src/components folder
+  // components: 'src/components/**/[A-Z]*.js',
 
   // Use this to test a single component.  Change it to the component you are testing and restart the styleguide server
   // Regex should be: 'src/components/NAME_OF_COMPONENT_FOLDER/[A-Z]*.js'
-	// components: 'src/components/**/[A-Z]*.js',
 	components: 'src/components/button/[A-Z]*.js',
 	defaultExample: true,
 	webpackConfig: {
