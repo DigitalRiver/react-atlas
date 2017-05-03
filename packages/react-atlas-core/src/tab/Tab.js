a5703f33af4efbace41f8a5ef73e8a48c26b1a29
pbalzano <pbalzano@digitalriver.com>
2017-05-03 17:24:45 -0300

    Tabs/Tab/TabContent components: refactor, fixes and styling

--- packages/react-atlas-core/src/tab/Tab.js
+++ packages/react-atlas-core/src/tab/Tab.js
@@ -1,8 +1,8 @@
 import React, { Component, PropTypes } from "react";
-import cx from 'classNames';
+import cx from "classNames";
 
 /**
- * Individual Tab component used within `<Tabs>`. Can be disabled, hidden and also sent an onActive event trigger. Children become `<TabContent>`.
+ * Individual Tab component used within `<Tabs>`. Can be disabled, hidden and also sent an onActive event trigger.
  */
 class Tab extends Component {
   componentDidUpdate(prevProps) {
@@ -13,24 +13,34 @@ class Tab extends Component {
 
   _handleClick = () => {
     if (!this.props.disabled && this.props.onClick) {
-      this.props.onClick();
+      this.props.onClick(this.props.index);
+    }
+  };
+
+  _handleActive = () => {
+    if (!this.props.disabled && this.props.onActive) {
+      this.props.onActive();
     }
   };
 
   render() {
     let { active, className, hidden, disabled, label, ...other } = this.props;
 
-    const classes = cx(
-      {
-        "label": !active && !hidden && !disabled,
-        active,
-        hidden,
-        disabled
-      }
-    );
+    const classes = cx({
+      "label": !active && !hidden && !disabled,
+      active,
+      hidden,
+      disabled
+    });
 
     return (
-      <label styleName={classes} onClick={this._handleClick} className={cx(className)}>
+      <label
+        {...other}
+        styleName={classes}
+        onClick={this._handleClick}
+        onActive={this._handleActive}
+        className={cx(className)}
+      >
         {label}
       </label>
     );
@@ -39,15 +49,45 @@ class Tab extends Component {
 
 Tab.propTypes = {
   "active": PropTypes.bool,
+  /**
+     * Defines a custom css class name.
+     * @examples 'tab', 'label'
+     */
   "className": PropTypes.string,
+  /**
+   * Determines if the tab is disabled.
+   * @examples '<Tab disabled/>'
+   */
   "disabled": PropTypes.bool,
+  /**
+   * Determines if the tab is hidden.
+   * @examples '<Tab hidden/>'
+   */
   "hidden": PropTypes.bool,
+  /**
+   * Determines label text on the tab navbar.
+   * @examples '<Tab label="Tab 1"/>'
+   */
   "label": PropTypes.string,
+  /**
+     * Sets a handler function to be executed when custom onActive event occurs.
+     * @examples <Tab onClick={this._tabOnActive}>(...)</Tabs>
+     */
   "onActive": PropTypes.func,
-  "onClick": PropTypes.func
+  /**
+     * Sets a handler function to be executed when onClick event occurs.
+     * @examples <Tab onClick={this._tabOnClick}>(...)</Tabs>
+     */
+  "onClick": PropTypes.func,
+  /**
+     * Tab index
+     * @examples 0,1,2,3,4
+     */
+  "index": PropTypes.number
 };
 
 Tab.defaultProps = {
+  "className": "",
   "active": false,
   "disabled": false,
   "hidden": false,
@@ -58,8 +98,7 @@ Tab.styleguide = {
   "category": "Layout",
   "index": "4.9",
   "wrappedExample": true,
-  "example": 
-    `
+  "example": `
 // Internal Methods {
 class TabsExample extends React.Component {
   state = {
@@ -105,7 +144,6 @@ class TabsExample extends React.Component {
 ReactDOM.render(<TabsExample />, mountNode);
 // }
 `
-  
 };
 
 export default Tab;
