8584afc892b3f832cca54e4026f5522679c1cbf2
U-MPLS\hbowden <2trill2spill@gmail.com>
2017-05-31 15:00:06 -0500

    Add click event prop, remove .bind(), remove ref, refactor css, remove wrapperRef from click event, fix eslint errors and format with preetier.

--- packages/react-atlas-core/src/checkbox/Checkbox.js
+++ packages/react-atlas-core/src/checkbox/Checkbox.js
@@ -1,59 +1,57 @@
 import React, { PropTypes } from "react";
-import cx from 'classNames';
+import cx from "classNames";
+import { InputCore } from "../input";
 
 /**
  * Simple component for a basic checkbox
  */
-class Checkbox extends React.Component {
+class Checkbox extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      "inputValue": '',
-      "checked": this.props.defaultChecked || false
+      checked: this.props.checked || false
     };
   }
 
   // Handles new checkbox clicks and sets value and checked status of hidden input
-  _clickHandler = (event) => {
-    if (this.wrapperRef && this.wrapperRef.contains(event.target)) {
-      if(this.state.inputValue === "off" || this.state.inputValue === "" || this.state.inputValue === null) {
-        this.setState({'inputValue': "on"});
-      } else {
-        this.setState({'inputValue': "off"});
-      }
+  _clickHandler = event => {
+    if (this.state.checked === true) {
+      this.setState({ checked: false });
+    } else {
+      this.setState({ checked: true });
+    }
 
-      if(this.state.defaultChecked === true){
-        this.setState({'checked': false})
-      } else {
-        this.setState({'checked': true})
-      }
-
+    /* Check if onClick has been passed, if so call it. */
+    if (this.props.onClick) {
+      this.props.onClick(event);
     }
   };
 
   render() {
-    const { title, label, checked, disabled, inline, className, defaultChecked, ...props } = this.props;
-    const inlineText = inline ? "inline" : "";
-    const controlStyle = cx({
-      "control": true,
-      "control--checkbox": true
-    })
-
+    const { title, label, disabled, inline, className } = this.props;
+    const display = inline ? "inline" : "block";
+    const controlStyle = cx("control", "control--checkbox", display);
     const title_label = title ? title : label;
-    const labelClasses = cx("control__indicator", inlineText);
+    const labelClasses = this.state.checked
+      ? cx("checked", display)
+      : cx("not_checked", display);
 
     return (
-      <label styleName={controlStyle} title={title_label} ref={(checkbox) => { this.wrapperRef = checkbox; }} className={cx(className)}>
-        <input
-            {...props}
-            name={label}
-            type="checkbox"
-            disabled={disabled}
-            value={this.state.inputValue}
-            defaultChecked={this.state.checked}
-            />
-
-        {label && <div styleName={labelClasses} onClick={this._clickHandler.bind(this)}><div styleName={"control__label"}>{label}</div></div>}
+      <label
+        styleName={controlStyle}
+        title={title_label}
+        className={cx(className)}
+      >
+        <InputCore
+          label={label}
+          type="checkbox"
+          disabled={disabled}
+          checked={this.state.checked}
+        />
+        {label &&
+          <div styleName={labelClasses} onClick={this._clickHandler}>
+            <div styleName={"label_left"}>{label}</div>
+          </div>}
       </label>
     );
   }
@@ -64,35 +62,40 @@ Checkbox.propTypes = {
    * Text for checkbox label
    * @examples 'Some Label'
    */
-  "label": PropTypes.string,
+  label: PropTypes.string,
   /**
    * A css class name that will be appended to the wrapping <label> element around the <input> and <span> elements.
    */
-  "className": PropTypes.string,
+  className: PropTypes.string,
   /**
    * If included, checkbox is disabled
    * @examples <Checkbox disabled />, <Checkbox disabled={true} />
    */
-  "disabled": PropTypes.bool,
+  disabled: PropTypes.bool,
   /**
    * If included, renders the checkbox and it's label inline, so it can be side-by-side to other content.
    */
-  "inline": PropTypes.bool,
+  inline: PropTypes.bool,
   /**
    * Text for checkbox label title. (i.e. "alt-text" for checkboxes, useful for accessibility). If not provided, will be label text.
    * @examples 'Some Title'
    */
-  "title": PropTypes.string,
+  title: PropTypes.string,
   /**
    * Defines if checkbox should be checked on load.
    */
-  "defaultChecked": PropTypes.bool,
+  checked: PropTypes.bool,
   /**
-   * Defines if checkbox should be checked on load.
+   * Allows user to pass a callback for click events.
    */
-  "checked": PropTypes.bool
+  onClick: PropTypes.func
 };
 
-Checkbox.defaultProps = { "className": "", "disabled": false, "inline": false, "checked": false };
+Checkbox.defaultProps = {
+  className: "",
+  disabled: false,
+  inline: false,
+  checked: false
+};
 
 export default Checkbox;
