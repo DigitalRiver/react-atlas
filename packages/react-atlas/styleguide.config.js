ff0eb682a442c9477479691508d773ff66172bd5
U-MPLS\hbowden <2trill2spill@gmail.com>
2017-07-05 10:16:57 -0500

    Add Table and TableHeader to the docsite.

--- packages/react-atlas/styleguide.config.js
+++ packages/react-atlas/styleguide.config.js
@@ -10,7 +10,7 @@ module.exports = {
 
   // Use this to test a single component.  Change it to the component you are testing and restart the styleguide server
   // Regex should be: 'src/components/NAME_OF_COMPONENT_FOLDER/[A-Z]*.js'
-  components: 'src/components/**/[Button|Avatar|Card|Checkbox|CheckboxGroup|Dropdown|Hint|Switch]*.js',
+  components: 'src/components/**/[Button|Avatar|Card|Checkbox|CheckboxGroup|Dropdown|Hint|Switch|Table|TableHeader]*.js',
   ignore: ['**/__tests__/**', '**/*.test.js', '**/*.test.jsx', '**/*.spec.js', '**/*.spec.jsx', '**/index.js'],
   contextDependencies: [
     '../react-atlas-core/src',
