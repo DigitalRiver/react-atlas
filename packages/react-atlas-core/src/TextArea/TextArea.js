1fd9708b442d5afe4b7ed0ecf514539332321b49
pbalzano <pbalzano@digitalriver.com>
2017-07-17 18:18:53 -0300

    Format & linting

--- packages/react-atlas-core/src/TextArea/TextArea.js
+++ packages/react-atlas-core/src/TextArea/TextArea.js
@@ -6,7 +6,7 @@ import cx from "classnames";
 class TextArea extends React.PureComponent {
   constructor(props) {
     super(props);
-	// Initial state
+    // Initial state
     this.state = {
       "value": props.value || "",
       "remaining": props.maxLength,
@@ -16,8 +16,8 @@ class TextArea extends React.PureComponent {
   }
 
   _handleChange = event => {
-  	event.persist();
-  	let value = event.target.value;
+    event.persist();
+    let value = event.target.value;
 
     if (this.props.maxLength) {
       // Keep difference between maxlength and input value in state for count
@@ -25,116 +25,125 @@ class TextArea extends React.PureComponent {
     }
 
     if (this.props.required) {
-    	// Set valid state depending on InputCore state
-    	this.setState({ "valid": this.inputRef.state.isValid });
+      // Set valid state depending on InputCore state
+      this.setState({ "valid": this.inputRef.state.isValid });
     }
 
     if (this.props.onChange) {
-    	// Execute app code
-    	this.props.onChange(event);
+      // Execute app code
+      this.props.onChange(event);
     }
-  }
+  };
 
   _handleFocus = event => {
-	this.setState({ "active": true });
-  }
+    this.setState({ "active": true });
+  };
 
   _handleBlur = event => {
-	this.setState({ "active": false });
-  }
+    this.setState({ "active": false });
+  };
 
   render() {
-  	const {
-  		maxLength,
-  		resizable,
-  		small,
-  		medium, 
-  		large,
-  		disabled, 
-  		hidden,
-  		...props
-  	} = this.props;
+    const {
+      maxLength,
+      resizable,
+      small,
+      medium,
+      large,
+      disabled,
+      hidden,
+      ...props
+    } = this.props;
 
-    let remainingCount = 
-		maxLength &&
-		<div styleName={cx("remainingCount")}>{maxLength - this.state.remaining}/{maxLength}</div>;
+    let remainingCount =
+      maxLength &&
+      <div styleName={cx("remainingCount")}>
+        {maxLength - this.state.remaining}/{maxLength}
+      </div>;
 
-	let wrapperClasses = cx({
-		hidden,
-		small,
-		medium,
-		large
-	}, "textareaWrapper");
+    let wrapperClasses = cx(
+      {
+        hidden,
+        small,
+        medium,
+        large
+      },
+      "textareaWrapper"
+    );
 
-	let textAreaClasses = cx({
-		resizable,
-		disabled,
-		"active": this.state.active,
-		"invalid": !this.state.valid
-	}, "textarea");
+    let textAreaClasses = cx(
+      {
+        resizable,
+        disabled,
+        "active": this.state.active,
+        "invalid": !this.state.valid
+      },
+      "textarea"
+    );
 
-  	return (
-  		<div 
-  			styleName={wrapperClasses}
-  			onFocus={this._handleFocus}
-  			onBlur={this._handleBlur}>
-	  		<InputCore 
-	  			multiline
-	  			maxLength={maxLength}
-	  			styleName={textAreaClasses}
-	  			onChange={this._handleChange}
-	  			disabled={disabled}
-	  			hidden={hidden}
-	  			ref={(node) => (this.inputRef = node)}
-	  			{...props}
-			/>
-			{remainingCount}
-		</div>
-	);
+    return (
+      <div
+        styleName={wrapperClasses}
+        onFocus={this._handleFocus}
+        onBlur={this._handleBlur}
+      >
+        <InputCore
+          multiline
+          maxLength={maxLength}
+          styleName={textAreaClasses}
+          onChange={this._handleChange}
+          disabled={disabled}
+          hidden={hidden}
+          ref={node => this.inputRef = node}
+          {...props}
+        />
+        {remainingCount}
+      </div>
+    );
   }
 }
 
 TextArea.PropTypes = {
-	/**
+  /**
 	 * Define a custom css class name.
 	 * @examples 'switch', 'switch-elem'
 	 */
-	"className": PropTypes.string,
-	/**
+  "className": PropTypes.string,
+  /**
 	 * Define a name for the textarea input.
 	 * @examples '<TextArea name="test"/>'
 	 */
-	"name": PropTypes.string,
-	/**
+  "name": PropTypes.string,
+  /**
 	 * Defines a small sized textarea.
 	 * @examples '<Switch small/>'
 	 */
-	"small": PropTypes.bool,
-	/**
+  "small": PropTypes.bool,
+  /**
 	 * Defines a medium sized textarea.
 	 * @examples '<Switch medium/>'
 	 */
-	"medium": PropTypes.bool,
-	/**
+  "medium": PropTypes.bool,
+  /**
 	 * Defines a large sized textarea.
 	 * @examples '<Switch large/>'
 	 */
-	"large": PropTypes.bool,
-	/**
+  "large": PropTypes.bool,
+  /**
 	 * Sets a handler function to be executed when onChange event occurs (at input element).
 	 * @examples <TextArea onChange={this.customOnChangeFunc}/>
 	 */
-	"onChange": PropTypes.func,
-	/**
+  "onChange": PropTypes.func,
+  /**
 	 * Determines if the textarea is disabled.
 	 * @examples '<TextArea disabled/>'
 	 */
-	"disabled": PropTypes.bool,
-	/**
+  "disabled": PropTypes.bool,
+  /**
 	 * Determines if the textarea is hidden.
 	 * @examples '<TextArea hidden/>'
 	 */
-	"hidden": PropTypes.bool
+  "hidden": PropTypes.bool
 };
 
 TextArea.defaultProps = {
