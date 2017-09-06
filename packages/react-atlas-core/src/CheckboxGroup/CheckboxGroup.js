1d258c261f84fa5596882f3d4419ac55595bb11c
Harrison <2trill2spill@gmail.com>
2017-09-06 10:22:59 -0500

    Add onChange prop to CheckBoxGroup. Remove groupHandleClick and use o… (#281)
    * Add onChange prop to CheckBoxGroup. Remove groupHandleClick and use onChange instead. Add onChange to CheckBoxGroup docs. Add value to checkboxes in readme onChange example.
    
    * Use arrow function instead of a classic function.

--- packages/react-atlas-core/src/CheckboxGroup/CheckboxGroup.js
+++ packages/react-atlas-core/src/CheckboxGroup/CheckboxGroup.js
@@ -23,7 +23,8 @@ class CheckboxGroup extends React.PureComponent {
   }
 
   // When child <Checkbox /> component changes state, update "totalChecked" value and verify against min or max props value
-  groupHandleClick = checked => {
+  handleChange = (value, event, isValid, checked) => {
+    event.persist();
     const newChecked = checked
       ? this.state.totalChecked + 1
       : this.state.totalChecked - 1;
@@ -36,6 +37,10 @@ class CheckboxGroup extends React.PureComponent {
       this.setState({ "groupError": true });
     } else {
       this.setState({ "groupError": false });
+    }
+
+    if(typeof this.props.onChange !== 'undefined') {
+      this.props.onChange(value, event, isValid, checked);
     }
   };
 
@@ -80,7 +85,7 @@ class CheckboxGroup extends React.PureComponent {
           child = cloneElement(child, {
             "inline": inline,
             "name": name,
-            "groupHandleClick": this.groupHandleClick,
+            "onChange": this.handleChange,
             "groupError": this.state.groupError
           });
           return child;
@@ -91,6 +96,12 @@ class CheckboxGroup extends React.PureComponent {
 }
 
 CheckboxGroup.propTypes = {
+  /**
+   * An optional callback that is fired when any checkbox in the
+   * CheckboxGroup changes value. The callback has four arguments.
+   * function(value, event, isValid, checked).
+   */
+  "onChange": PropTypes.func,
   /**
    * Anything that can be in a checkbox group. Typically only includes Checkbox components and a header.
    * @examples '<CheckboxGroup><Checkbox/><Checkbox/></CheckboxGroup>'
