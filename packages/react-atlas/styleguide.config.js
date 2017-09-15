20a23e35385bf92c8ad51c6674fb53864f689a57
Magnum <Magneticmagnum@users.noreply.github.com>
2017-09-15 10:43:09 -0500

    Update styleguide.config.js
    Adding all components to render in styleguidist

--- packages/react-atlas/styleguide.config.js
+++ packages/react-atlas/styleguide.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ],
   // Use this to test a single component.  Change it to the component you are testing and restart the styleguide server
   // Regex should be: 'src/components/NAME_OF_COMPONENT_FOLDER/[A-Z]*.js'
-  components: 'src/components/**/[Avatar|Button|Card|Checkbox|CheckboxGroup|Dialog|Dropdown|Form|Hint|Input|Modal|ProgressBar|Radio|RadioGroup|Switch|Table|TableHeader|TextArea|TextField|Tooltip]*.js',
+  components: 'src/components/**/[A-Z]*.js',
 
   ignore: ['**/__tests__/**', '**/*.test.js', '**/*.test.jsx', '**/*.spec.js', '**/*.spec.jsx', '**/index.js'],
   contextDependencies: [
