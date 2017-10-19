7277ab780d6e11dba7591da7c7dd650dd30b2427
U-MPLS\hbowden <2trill2spill@gmail.com>
2017-10-19 13:38:43 -0500

    Add license validator.

---
+++ scripts/validateLicense.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+var spawn = require("cross-spawn");
+var path = require("path");
+var packages = [path.join(__dirname, "../packages/react-atlas"), path.join(__dirname, "../packages/react-atlas-core"), path.join(__dirname, "../packages/react-atlas-default-theme"), path.join(__dirname, "../packages/light-table"), path.join(__dirname, "../")];
+
+packages.forEach(function(pack) {
+  spawn.sync('node-license-validator', ['--dir', pack, '--deep', '--allow-licenses', 'ISC', 'MIT', 'BSD', 'BSD-3-Clause', 'BSD-2-Clause', 'WTFPL', 'BSD-like', 'MIT/X11', 'Public Domain', 'BSD-new', 'Artistic-2.0', 'Unlicense', ' CC0-1.0'], {
+    env: process.env,
+    cwd: pack,
+    stdio: "inherit"
+  });
+ });
