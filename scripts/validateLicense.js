0720f5cf9cf2f67f919f2ee34eb13afd40560c1f
U-MPLS\hbowden <2trill2spill@gmail.com>
2017-10-19 14:02:52 -0500

    use correct script name, and don't checks deps deps/

--- scripts/validateLicense.js
+++ scripts/validateLicense.js
@@ -4,7 +4,7 @@ var path = require("path");
 var packages = [path.join(__dirname, "../packages/react-atlas"), path.join(__dirname, "../packages/react-atlas-core"), path.join(__dirname, "../packages/react-atlas-default-theme"), path.join(__dirname, "../packages/light-table"), path.join(__dirname, "../")];
 
 packages.forEach(function(pack) {
-  spawn.sync('node-license-validator', ['--dir', pack, '--deep', '--allow-licenses', 'ISC', 'MIT', 'BSD', 'BSD-3-Clause', 'BSD-2-Clause', 'WTFPL', 'BSD-like', 'MIT/X11', 'Public Domain', 'BSD-new', 'Artistic-2.0', 'Unlicense', ' CC0-1.0'], {
+  spawn.sync('node-license-validator', ['--dir', pack, '--allow-licenses', 'ISC', 'MIT', 'BSD', 'BSD-3-Clause', 'BSD-2-Clause', 'WTFPL', 'BSD-like', 'MIT/X11', 'Public Domain', 'BSD-new', 'Artistic-2.0', 'Unlicense', ' CC0-1.0'], {
     env: process.env,
     cwd: pack,
     stdio: "inherit"
