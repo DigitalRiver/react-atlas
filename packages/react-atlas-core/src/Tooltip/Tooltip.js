ef44882fae2fba128ff90bd83328a5139eac4acc
Eric Sewalson <esewalson@digitalriver.com>
2017-10-26 13:43:53 -0500

    fixes #334

--- packages/react-atlas-core/src/Tooltip/Tooltip.js
+++ packages/react-atlas-core/src/Tooltip/Tooltip.js
@@ -54,7 +54,7 @@ class Tooltip extends React.PureComponent {
     });
 
     let TooltipContainer = this.state.active ? (
-      <span>{this.state.tooltip}</span>
+      <span styleName={'tooltipContent'}>{this.state.tooltip}</span>
     ) : null;
 
     let Icon = icon && !children ? <i className={cx(icon)} /> : null;
