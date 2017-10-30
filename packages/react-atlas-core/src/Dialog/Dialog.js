24f88f5239413093562ab66d079c224c76b0cbca
dharper <dharper@digitalriver.com>
2017-10-30 10:02:01 -0500

    Adding Modal and Button  imports to Dialog

--- packages/react-atlas-core/src/Dialog/Dialog.js
+++ packages/react-atlas-core/src/Dialog/Dialog.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
+import { ModalCore } from "../Modal";
 
 class Dialog extends React.PureComponent {
   _getButtonContent = () => {
