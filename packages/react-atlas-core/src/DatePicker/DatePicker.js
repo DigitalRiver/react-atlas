eb42e34488f70a9dc58f4e2bb175d5d9d8890e26
magneticmagnum <mmmagnum@gmail.com>
2017-11-30 17:32:36 +0000

    Added Selected prop, which will allow initial state of a date other than now().  Closes #530

--- packages/react-atlas-core/src/DatePicker/DatePicker.js
+++ packages/react-atlas-core/src/DatePicker/DatePicker.js
@@ -8,7 +8,7 @@ class DatePicker extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      "startDate": moment()
+      "startDate": moment(this.props.selected)|| moment()
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -20,6 +20,16 @@ class DatePicker extends React.PureComponent {
     });
     if (typeof this.props.onChange !== "undefined") {
       this.props.onChange(date, event, true);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log("CWRP: ", nextProps)
+    if (
+      typeof nextProps.selected !== "undefined" &&
+      nextProps.selected !== this.state.selected
+    ) {
+      this.setState({ "startDate": moment(nextProps.selected) });
     }
   }
 
@@ -43,6 +53,10 @@ DatePicker.propTypes = {
   "format": PropTypes.string,
   "value": PropTypes.string,
   "onChange": PropTypes.func,
+  /**
+   * The currently selected Date.  You can use any format accepted by Moment.js
+   */
+  "selected": PropTypes.string,
   /**
    * Pass inline styles here.
    */
