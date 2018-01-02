2c199f2dd90f0699eb910e2d41141d4fbc8c80c7
U-MPLS\hbowden <2trill2spill@gmail.com>
2018-01-02 15:01:24 -0600

    Merge with master add missing comma.

--- packages/react-atlas-core/src/TextField/TextField.js
+++ packages/react-atlas-core/src/TextField/TextField.js
@@ -322,7 +322,7 @@ TextField.propTypes = {
   /** Set if you want a link button next to the textfield header. **/
   "link": PropTypes.bool,
   /** Set if you want the link button to the right of the textfield header. **/
-  "linkRight": PropTypes.bool
+  "linkRight": PropTypes.bool,
     /** The text of the link button. **/
   "linkText": PropTypes.string,
     /** Callback to call when link buttonis clicked. **/
