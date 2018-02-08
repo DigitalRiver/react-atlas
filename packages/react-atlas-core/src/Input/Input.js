13ae75d25320a56148005ac3ca0956ac3089c1cc
dharper <dharper@digitalriver.com>
2018-02-07 23:38:24 -0600

    Changing className to styleName so that composes works properly. Updating some styling for disabled inputs.

--- packages/react-atlas-core/src/Input/Input.js
+++ packages/react-atlas-core/src/Input/Input.js
@@ -15,7 +15,10 @@ class Input extends React.PureComponent {
 
     // Initial state
     this.state = {
-      "value": (typeof props.value === "undefined" || props.value === null) ? "" : props.value,
+      "value":
+        typeof props.value === "undefined" || props.value === null
+          ? ""
+          : props.value,
 
       "errorText": "This field is required."
     };
@@ -54,12 +57,12 @@ class Input extends React.PureComponent {
         "isValid": nextProps.isValid
       });
     }
-    if(nextProps.value && nextProps.value !== this.props.value) {
+    if (nextProps.value && nextProps.value !== this.props.value) {
       this.setState({
         "value": nextProps.value
       });
     }
-}
+  }
 
   _updateMaskSelection = () => {
     this.mask.selection = utils.getSelection(this.input);
@@ -231,8 +234,8 @@ class Input extends React.PureComponent {
 
     let valid = this._validate(inputValue);
 
-    if(this.props.uppercase){
-        inputValue = inputValue.toUpperCase();
+    if (this.props.uppercase) {
+      inputValue = inputValue.toUpperCase();
     }
 
     if (valid !== false) {
@@ -273,6 +276,12 @@ class Input extends React.PureComponent {
     let isRadio = type === "radio";
     const isInput = isCheckbox || isRadio ? false : true;
 
+    let containerClasses = cx({
+      "max": !small && !medium && !large && !isRadio && !isCheckbox,
+      "container": true,
+      "container-disabled": disabled
+    });
+
     let inputClasses = cx({
       "input": isInput,
       "checkbox": isCheckbox,
@@ -281,7 +290,7 @@ class Input extends React.PureComponent {
       "small": small,
       "medium": medium,
       "large": large,
-      "max": !small && !medium && !large,
+      "max": !small && !medium && !large && !isRadio && !isCheckbox,
       disabled,
       hidden
     });
@@ -294,7 +303,7 @@ class Input extends React.PureComponent {
       "onPaste": this._handlePaste
     };
 
-    let inputElement = multiline ?
+    let inputElement = multiline ? 
       <textarea
         id={id}
         name={name}
@@ -305,7 +314,7 @@ class Input extends React.PureComponent {
         className={cx(className)}
         onChange={this._handleChange}
       />
-     :
+     : 
       <input
         type={type}
         name={name}
@@ -321,11 +330,11 @@ class Input extends React.PureComponent {
       />
     ;
 
-    let errorTextElement = this.state.errorText &&
-      <span className={"ra_Input__error"}>{this.state.errorText}</span>
+    let errorTextElement = this.state.errorText && 
+      <span styleName={cx("error")}>{this.state.errorText}</span>
     ;
 
-    return isCheckbox ?
+    return isCheckbox ? 
       <input
         style={style}
         type="checkbox"
@@ -336,8 +345,8 @@ class Input extends React.PureComponent {
         checked={checked}
         {...eventHandlers}
       />
-     :
-      <div className={"ra_Input__container"}>
+     : 
+      <div styleName={containerClasses}>
         {inputElement}
         {this.state.isValid ? null : errorTextElement}
       </div>
@@ -472,9 +481,9 @@ Input.propTypes = {
    * Converts all entered text to uppercase.
    */
   "uppercase": PropTypes.bool,
-   /**
-    * Specifies the amount of rows
-    */
+  /**
+   * Specifies the amount of rows
+   */
   "rows": PropTypes.number
 };
 
