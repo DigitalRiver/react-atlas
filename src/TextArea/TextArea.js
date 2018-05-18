6e904ff649822e383ce3d2506663d331e5fcd029
cnoree <cnoree@digitalriver.com>
2018-05-18 10:39:40 -0500

    Refactored TextArea to fit in with 0.1.0-rc branch

--- src/TextArea/TextArea.js
+++ src/TextArea/TextArea.js
@@ -1,201 +1,228 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Input from "../Input";
+import messages from "../utils/messages.js";
 import cx from "classnames";
-import Tooltip from "./../Tooltip";
+import CSSModules from "react-css-modules";
+import styles from "./TextArea.css";
 
 export class TextArea extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    let isValid;
-    if (typeof props.isValid === "undefined") {
-      isValid = true;
-    } else if (props.isValid === false) {
-      isValid = props.isValid;
-    } else {
-      isValid = props.isValid;
-    }
-
-    if (this.props.tooltip && !this.props.label) {
-      throw "Tooltip requires Label";
-    }
-
     // Initial state
     this.state = {
-      "value":
-        typeof props.value === "undefined" || props.value === null
-          ? ""
-          : props.value,
-      "remaining": props.maxLength,
+      "value": props.value,
       "active": false,
-      "isValid": isValid
+      "status": props.status || null,
+      "message": props.message || null
     };
   }
+
   componentWillReceiveProps(nextProps) {
+    if (nextProps.value && nextProps.value !== this.props.value) {
+      this.setState({
+        "value": nextProps.value
+      });
+    }
     if (
-      typeof nextProps.isValid !== "undefined" &&
-      nextProps.isValid !== this.state.valid
+      typeof nextProps.status !== "undefined" &&
+        nextProps.status !== this.props.status ||
+      typeof nextProps.message !== "undefined" &&
+        nextProps.message !== this.props.message
     ) {
-      this.setState({ "valid": nextProps.isValid });
+      this.setState({
+        "status": nextProps.status,
+        "message": nextProps.message
+      });
     }
   }
 
-  _handleChange = (value, event, isValid) => {
-    event.persist();
+  _eventHandlers = (e, change) => {
+    const data = {
+      "value": this.state.value,
+      "status": this.state.status
+    };
 
-    if (this.props.maxLength) {
-      // Keep difference between maxlength and input value in state for count
-      this.setState({ "remaining": this.props.maxLength - value.length });
+    if (!change && this.props.onBlur) {
+      this.props.onBlur(e, data);
     }
 
-    // Set value and isValid state depending on InputCore state
-    this.setState({
-      "value": value,
-      "isValid": isValid
+    if (change && this.props.onChange) {
+      this.props.onChange(e, data);
+    }
+  };
+
+  _validate = (e, inputValue, change) => {
+    let status = null;
+    let message = null;
+
+    /* Execute custom validator and change state and error messages accordingly */
+    if (typeof this.props.valid === "function") {
+      let validationObject = this.props.valid(inputValue);
+      if (validationObject === false) {
+        validationObject = { "status": "error", "message": null };
+      }
+      if (typeof validationObject === "undefined") {
+        validationObject = { "status": null, "message": null };
+      }
+      status = validationObject.status;
+      message = validationObject.message;
+    } else if (
+      !inputValue.length &&
+      (this.props.required ||
+        typeof this.props.required === "string" && this.props.required === "")
+    ) {
+      /* If the field is required, and it has no value, change state and display error message */
+      message = messages.requiredMessage;
+      status = "error";
+    }
+
+    this.setState(
+      {
+        "status": status,
+        "message": message,
+        "value": inputValue,
+        "active": change
+      },
+      () => {
+        this._eventHandlers(e, change);
+      }
+    );
+  };
+
+  _handleChange = e => {
+    e.persist();
+    let value = e.target.value;
+
+    if (this.props.uppercase) {
+      value = value.toUpperCase();
+    }
+
+    this._validate(e, value, true);
+  };
+
+  _handleFocus = e => {
+    e.persist();
+    this.setState({ "active": true }, function() {
+      if (typeof this.props.onFocus !== "undefined") {
+        this.props.onFocus(e, {
+          "value": this.state.value,
+          "status": this.state.status
+        });
+      }
     });
-
-    if (this.props.onChange) {
-      // Execute app code
-      this.props.onChange(value, event, isValid);
-    }
   };
 
-  _handleFocus = () => {
-    this.setState({ "active": true });
-  };
-
-  _handleBlur = () => {
-    this.setState({ "active": false });
+  _handleBlur = e => {
+    e.persist();
+    const value = e.target.value;
+    this._validate(e, value, false);
   };
 
   render() {
     const {
-      id,
-      name,
-      label,
-      placeholder,
-      maxLength,
-      resizable,
-      small,
-      medium,
-      large,
-      required,
-      disabled,
-      hidden,
       className,
+      disabled,
+      id,
+      inline,
+      label,
+      leftLabel,
+      required,
       style,
       tooltip,
-      tooltipRight,
-      leftLabel,
-      rows,
+      tooltipPosition,
+      type,
       ...others
     } = this.props;
 
-    let tooltipClasses = cx({
-      "tooltipAlignment": true,
-      "tooltipRight": tooltipRight
-    });
+    const reqText = typeof required === "string" ? required : "*";
 
-    let tooltipInternalClasses = cx({
-      "ra_Tooltip__tooltip": true,
-      "ra_Tooltip__active": true,
-      "ra_Tooltip__tooltip-right": true,
-      "ra_Tooltip__tooltipContent": true,
-      "ra_Tooltip__block": true
+    let wrapperClasses = cx({
+      leftLabel,
+      inline,
+      "textareaWrapper": true
     });
-
-    let remainingCount = maxLength && 
-      <div styleName={"remainingCount"}>
-        {maxLength - this.state.remaining}/{maxLength}
-      </div>
-    ;
-    const forId = id !== "" && name !== "" ? id : "";
 
     let labelClasses = cx({
-      "leftLabel": leftLabel,
-      "label": true,
-      "labelFont": true,
-      "labelSpacing": true
+      "labelSpacing": !leftLabel,
+      "label": true
     });
 
-    let textAreaLabel = label && 
-      <label styleName={labelClasses} title={label} htmlFor={forId}>
-        <span styleName={"headerFont"}>{label}</span>
-        {required && <span styleName={"error_text"}> *</span>}
-        {tooltip && 
-          <span styleName={tooltipClasses}>
-            <Tooltip
-              className={tooltipInternalClasses}
-              text={tooltip}
-              position="top"
-            />
-          </span>
-        }
-      </label>
+    let labelContainerClasses = cx({
+      "tooltipRight": tooltipPosition === "right",
+      "labelContainer": true
+    });
+
+    let labelPadding = cx({
+      "verticalPadding": !leftLabel,
+      "horizontalPadding": true
+    });
+
+    let inputClasses = cx({
+      "textarea": true,
+      "active": this.state.active,
+      "error": this.state.status === "error",
+      "success": this.state.status === "success",
+      "warning": this.state.status === "warning",
+      disabled
+    });
+
+    let messageClasses = cx({
+      "success_message": this.state.status === "success",
+      "error_message": this.state.status === "error",
+      "warning_message": this.state.status === "warning"
+    });
+
+    const requiredClasses = cx({
+      "required": true,
+      "required_error": this.state.status === "error"
+    });
+
+    let textAreaLabel = (label || tooltip) && 
+      <div styleName={labelContainerClasses}>
+        <div styleName={cx(labelPadding)}>
+          {label && 
+            <label styleName={labelClasses} htmlFor={id}>
+              {label}
+            </label>
+          }
+          {required &&
+            required !== "" && 
+              <span styleName={requiredClasses}> {reqText}</span>
+            }
+        </div>
+        {tooltip}
+      </div>
     ;
 
-    let wrapperClasses = cx(
-      {
-        hidden,
-        small,
-        medium,
-        large
-      },
-      "textareaWrapper"
-    );
-
-    let textAreaClasses = cx(
-      {
-        resizable,
-        disabled,
-        "active": this.state.active,
-        "invalid": !this.state.isValid
-      },
-      "textarea"
-    );
-
-    const contentClasses = cx({
-      "content": true,
-      "leftLabelContent": leftLabel,
-      "fullWidth": true
-    });
-
     return (
-      <div
-        style={style}
-        styleName={wrapperClasses}
-        onFocus={this._handleFocus}
-        onBlur={this._handleBlur}
-        className={cx(className)}
-      >
+      <div styleName={wrapperClasses}>
         {textAreaLabel}
-        <div styleName={contentClasses}>
-          <Input
-            {...others}
-            id={id}
-            multiline
-            rows={rows}
-            name={name}
-            placeholder={placeholder}
-            maxLength={maxLength}
-            styleName={textAreaClasses}
-            onChange={this._handleChange}
-            required={required}
-            disabled={disabled}
-            hidden={hidden}
-            value={this.state.value}
-            isValid={this.state.isValid}
-          />
-        </div>
-        {remainingCount}
+        <textarea
+          {...others}
+          id={id}
+          type={type}
+          value={this.state.value}
+          styleName={inputClasses}
+          style={style}
+          className={className}
+          required={required}
+          disabled={disabled}
+          onChange={this._handleChange}
+          onFocus={this._handleFocus}
+          onBlur={this._handleBlur}
+        />
+        {this.state.message !== null && 
+          <div>
+            <span styleName={messageClasses}>{this.state.message}</span>
+          </div>
+        }
       </div>
     );
   }
 }
 
 TextArea.propTypes = {
-  "isValid": PropTypes.bool,
   /** An Object, array, or string of CSS classes to apply to TextArea.*/
   "className": PropTypes.oneOfType([
     PropTypes.string,
@@ -203,101 +230,81 @@ TextArea.propTypes = {
     PropTypes.array
   ]),
   /**
-   * Define a name for the textarea input.
-   * @examples '<TextArea id="test"/>'
+   * Determines if the TextArea is disabled.
+   * @examples '<TextArea disabled/>'
    */
+  "disabled": PropTypes.string,
+  /** Define an id for the text input.*/
   "id": PropTypes.string,
+  /** Sets whether or not TextArea will display as inline */
+  "inline": PropTypes.bool,
   /**
-   * Define a name for the textarea input.
-   * @examples '<TextArea name="test"/>'
-   */
-  "name": PropTypes.string,
-  /**
-   * Define a value for the textarea input.
-   * @examples '<TextArea value="test"/>'
-   */
-  "value": PropTypes.string,
-  /**
-   * Define a title or label to be displayed above the textarea.
+   * Define a label to be displayed above the TextArea.
    * @examples '<TextArea label="test"/>'
    */
   "label": PropTypes.string,
   /**
-   * Defines a resizable textarea. Default: true.
-   * @examples '<TextArea resizable={false}/>'
+   * Allows user to move the label to the left of the TextArea instead of above it
    */
-  "resizable": PropTypes.bool,
+  "leftLabel": PropTypes.bool,
   /**
-   * Defines a small sized textarea.
-   * @examples '<TextArea small/>'
+   * Sets the status message to display below the TextArea. The color of the message will be determined by the value of the "status" property.
+   * @examples '<TextArea message="Incorrect answer" status="error" />'
    */
-  "small": PropTypes.bool,
+  "message": PropTypes.string,
   /**
-   * Defines a medium sized textarea.
-   * @examples '<TextArea medium/>'
+   * A callback that fires onBlur.
    */
-  "medium": PropTypes.bool,
-  /**
-   * Defines a large sized textarea.
-   * @examples '<TextArea large/>'
-   */
-  "large": PropTypes.bool,
-  /**
-   * Sets a maximum character length that will be validated onChange.
-   * @examples '<TextArea maxLength={25}/>'
-   */
-  "maxLength": PropTypes.number,
-  /**
-   * Defines placeholder text.
-   * @examples '<TextArea placeholder="test input"/>'
-   */
-  "placeholder": PropTypes.string,
+  "onBlur": PropTypes.func,
   /**
    * Sets a handler function to be executed when onChange event occurs (at input element).
    * @examples <TextArea onChange={this.customOnChangeFunc}/>
    */
   "onChange": PropTypes.func,
   /**
-   * Sets the field as required. Will be validated onChange.
+   * A callback that fires onFocus.
+   */
+  "onFocus": PropTypes.func,
+  /**
+   * Sets the TextArea as required. Will be validated onChange. Accepts a boolean or a string. If a string is passed it will be displayed instead of the traditional * next to the field label.
    * @examples '<TextArea required/>'
    */
-  "required": PropTypes.bool,
+  "required": PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
   /**
-   * Determines if the textarea is disabled.
-   * @examples '<TextArea disabled/>'
+   * Sets the status of the TextArea. Options are null, "success", "error", and "warning".
+   * @examples '<TextArea status="error" />'
    */
-  "disabled": PropTypes.bool,
-  /**
-   * Determines if the textarea is hidden.
-   * @examples '<TextArea hidden/>'
-   */
-  "hidden": PropTypes.bool,
-  /**
-   * Set the tooltip value *
-   */
-  "tooltip": PropTypes.string,
+  "status": PropTypes.string,
   /** Pass inline styling here. */
   "style": PropTypes.object,
+  /** Sets an element to be displayed along with the TextArea. Traditionally used with the Tooltip component, but will accept any component or HTML element. */
+  "tooltip": PropTypes.node,
+  /** Sets the position of the embedded Tooltip. Defaults to "right", any other value will move it next to the label. */
+  "tooltipPosition": PropTypes.string,
   /**
-   * Positions the tooltip to the right. Default true.
+   * Define a type for the text input. Default is "text".
+   * @examples '<TextArea type="password"/>'
    */
-  "tooltipRight": PropTypes.bool,
+  "type": PropTypes.string,
   /**
-   * Allows user to move the label to the left of the TextArea instead of having it on top
+   * Converts all entered text to uppercase.
    */
-  "leftLabel": PropTypes.bool,
+  "uppercase": PropTypes.bool,
   /**
-   * Specifies the amount of rows
+   * Sets a handler function to be executed and validate against. Will override the required property (you can still use the required prop to add a required indicator next to the label) and must return an object with a status (Options: null, "success", "error") and a message (Options: null or string)
+   * @examples '<TextArea valid={this.customValidator}/>'
    */
-  "rows": PropTypes.number
+  "valid": PropTypes.func,
+  /**
+   * Define a default value for the text input.
+   * @examples '<TextArea value="TextArea value here"/>'
+   */
+  "value": PropTypes.string
 };
 
 TextArea.defaultProps = {
-  "className": "",
-  "resizable": true,
-  "disabled": false,
-  "hidden": false,
-  "tooltipRight": true
+  "type": "text",
+  "tooltipPosition": "right"
 };
 
-export default TextArea;
+export default CSSModules(TextArea, styles, { "allowMultiple": true });
