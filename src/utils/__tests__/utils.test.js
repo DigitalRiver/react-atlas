f4d3255904703226f0ee9d267c08e481fe56404c
stuller <stuller@digitalriver.com>
2018-11-21 14:47:35 -0600

    adding unit tests for utils

--- src/utils/__tests__/utils.test.js
+++ src/utils/__tests__/utils.test.js
@@ -1,6 +1,61 @@
 import utils from "../../utils/utils";
+import React from "react";
 
 describe("Testing utilities", () => {
+  it("Test round() correctly rounds up", function() {
+    expect(utils.round(2.55555, 0)).toBe(3);
+    expect(utils.round(2.55555, 1)).toBe(2.6);
+    expect(utils.round(2.55555, 2)).toBe(2.56);
+    expect(utils.round(2.55555, 3)).toBe(2.556);
+  });
+
+  it("Test round() correctly rounds down", function() {
+    expect(utils.round(2.11111, 0)).toBe(2);
+    expect(utils.round(2.11111, 1)).toBe(2.1);
+    expect(utils.round(2.11111, 2)).toBe(2.11);
+    expect(utils.round(2.11111, 3)).toBe(2.111);
+  });
+
+  it("Test round() returns NaN if not given a number", function() {
+    expect(utils.round("foo", 0)).toBe(NaN);
+  });
+
+  it("Test getViewport() returns an object", function() {
+    expect(typeof utils.getViewport()).toBe("object");
+    expect(utils.getViewport().hasOwnProperty("height")).toBe(true);
+    expect(utils.getViewport().hasOwnProperty("width")).toBe(true);
+    expect(utils.getViewport().height).toEqual(
+      window.innerHeight || document.documentElement.offsetHeight
+    );
+    expect(utils.getViewport().width).toEqual(
+      window.innerWidth || document.documentElement.offsetWidth
+    );
+  });
+
+  it("Test cloneObject() returns a cloned object", function() {
+    let myObject = {
+      "foo": "foo",
+      "bar": "bar"
+    };
+
+    expect(typeof utils.cloneObject(myObject)).toBe("object");
+    expect(utils.cloneObject(myObject)).toEqual(myObject);
+  });
+
+  it("getRequiredText creates span with required text", function() {
+    let reqText = "foo";
+    expect(utils.getRequiredText(reqText)).toEqual(
+      <span styleName="error_text">foo</span>
+    );
+  });
+
+  it("getErrorMessage creates div with error message text", function() {
+    let error = "foo";
+    expect(utils.getErrorMessage(error)).toEqual(
+      <div styleName="error_message">foo</div>
+    );
+  });
+
   it("Test getCompName()", function() {
     // expect(utils).not.toBe(undefined); Commented out due to linting error
 
